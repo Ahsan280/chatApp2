bb31bd0557db9451d4f15c4e9812b01b045a54c8
Ahsan280 <ahsanrehman281@gmail.com>
2024-04-19 10:52:49 +0500

    vercel-5

--- mychatproject/settings.py
+++ mychatproject/settings.py
@@ -72,8 +72,8 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'mychatproject.wsgi.application'
-ASGI_APPLICATION = 'mychatproject.asgi.application'
+WSGI_APPLICATION = 'mychatproject.wsgi.app'
+ASGI_APPLICATION = 'mychatproject.asgi.app'
 
 
 # Database
